e852c2cd6dd9a8756c52a01c00a39c65286337f0
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2019-11-03 22:27:59 -0500

    Update timelines.py

--- timelines/timelines.py
+++ timelines/timelines.py
@@ -7,18 +7,16 @@ import csv
 #painkilleralready.info didn't have all timelines available.
 #Youtube has an api but didn't feel like learning it.
 def main(url):
-
     with open(url, 'r') as f:
         episodes = json.load(f)['episodes']
 
     with open('Timeline.txt','w+',encoding='utf-8') as f:
         for episode in episodes:
             url = f'https://www.youtube.com/watch?v={episodes[episode]["YouTube"]}'
-            if float(episode.split('_')[1]) > 329:
-                continue
+        
             print(episode)
             print(url)
-  
+
             htmlStr = requests.get(url).text
             htmlStr = htmlStr.split('<')
 
@@ -32,14 +30,15 @@ def main(url):
                     timelineCheck = [str(i) for i in range(10)]
                     timeline = [i.rstrip('\\') for i in description if i[0] in timelineCheck] #If the line starts with str 1-9 then it's part of timeline.
 
-                    hasTimelineFlag= False 
+                    hasTimelineFlag= False
+                    lastStart = []
                     for i, stamp in enumerate(timeline):
                         #pprint(stamp)
-                        if stamp[0:6] == '0:00:0': #Some timelines did not start at 0
-                            start=i
+                        if stamp[0:6] == '0:00:0': #Some timelines do not start at 0
+                            lastStart.append(i)
                             hasTimelineFlag = True
                         if stamp[0:5] == '0:00 ': #Some timelines started with a minute and second level accuracy
-                            start=i
+                            lastStart.append(i)
                             hasTimelineFlag = True
                             break
 
@@ -47,7 +46,7 @@ def main(url):
                         timeline[-1] = timeline[-1].split('\\"}')[0]
                         
                         finalTimeline = []
-                        for i in timeline[start:]:
+                        for i in timeline[lastStart[-1]:]:
                             fixed = i.split('\\\\u0026')
                             fixed = '&'.join(fixed)
 
@@ -55,13 +54,12 @@ def main(url):
                             fixed = ''.join(fixed)
                             finalTimeline.append(fixed)
 
-                        f.write(episode + '\n')
+                        f.write(episode + ' - ' + url + '\n')
                         for i in finalTimeline:
                             f.write('\t' + i+'\n')
                         f.write('\n')
 
                     else:
                         f.write(episode + ' - ' + url + '\n\n')
-
                 
 main('pkaInfo.json')
