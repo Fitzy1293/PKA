cb6fd5bc17318fb08c01ef52484efdb1257e5e40
fitzy1293 <omfitzgerald61298@gmail.com>
2020-11-05 13:46:42 -0500

    fix

--- pkaURLs.py
+++ pkaURLs.py
@@ -5,24 +5,49 @@ import requests
 import sys, os
 from bs4 import BeautifulSoup
 
-
+#run ytdl-json
 
 if __name__ == '__main__':
     titles = []
     descriptions = []
+    jsonFiles = [file for file in os.listdir('json')]
+    for i in jsonFiles:
+        if i.startswith(('PKA','Painkiller')):
+            words = ''.join(i.split('-')[:-1])
+            titleWords = words.split()
 
-    for file in os.listdir('json'):
+            foundInt = False
+            for j in titleWords:
+                if foundInt:
+                    break
+                try:
+                    epNum = float(j)
+                    titles.append((epNum, i))
+                    break
+                except Exception as e:
+                    pass
 
-        vidObject = open(f'json/{file}', 'r').read()
+    sortedTitles = sorted(titles, key=lambda x: (x[0], x[1].lower()))
+    sortedTitles.reverse()
+    pprint(sortedTitles)
+    for i in sortedTitles:
+
+        vidObject = open(f'json/{i[1]}', 'r').read()
         vidDict = json.loads(vidObject)
         description = vidDict['description']
-        title = vidDict['fulltitle']
-
         descriptionFmt = '\t' + '\n\t'.join(description.split('\n'))
         descriptions.append('\t' + '\n\t'.join(description.split('\n')))
-        titles.append('\n'.join(title.split('\n')))
+
+
     with open('descriptions.txt', 'w+') as f:
-        for i in reversed(range(len(descriptions))):
-            f.write(titles[i])
+        for i in reversed(range(len(sortedTitles))):
+            f.write(sortedTitles[i][1])
             f.write('\n' + descriptions[i] + '\n\n')
         f.write('\n')
+
+    foundEps = [i[0] for i in sortedTitles]
+    for i in range(1, 516):
+        if i not in foundEps:
+            print(i)
+
+    print(len(sortedTitles))
