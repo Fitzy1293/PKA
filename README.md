1b34542a9f180e7782ceaa084cb6dce3c892737e
fitzy1293 <omfitzgerald61298@gmail.com>
2020-11-05 16:57:55 -0500

    Releasable

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # PKA
-Things for the PKA podcast. Powered by Kyle's crippling adderall addiction. 
 
-Random stuff.
+Concatenate LegitimateRage's descriptions into one file. \
+[This playlist](https://www.youtube.com/playlist?list=PL568FBE856C240972) had the most complete list of PKA urls.
 
-Code to make an audio archive of every PKA; a single file containing each PKA topic from each episode. 
+To get the descriptions I used yt-dl (R.I.P their github page) with the command
+
+`youtube-dl "$(cat playlist-url)" --write-info-json --skip-download -i`
+
+Some were still not in that playlist so I ran this with the missing episode's URL as the argument. 
+
+`cd json && youtube-dl "$1" --write-info-json --skip-download -i && cd ..`
+
+
+The code mostly deals with finding the episode number from the json file names to sort.  
--- pkaURLs.py
+++ pkaURLs.py
@@ -4,50 +4,102 @@ import json
 import requests
 import sys, os
 from bs4 import BeautifulSoup
+import sys
 
-#run ytdl-json
+def handleEpFromTitle(title):
+    missingEps = (101, 169, 306, 350, 355) # Deleted.
+
+    if title.startswith('10 Year Anniversary'): 
+        return (488, title)
+    elif title.startswith('Painkiller Already 169'): # Miss-titled. 169 is missing.
+        return (168, 'Painkiller Already 169-NRrm-MomDD8.info.json')
+    elif title.startswith('Painkiller Already160'):
+        return (160, 'Painkiller Already160 w_ Joe Lauzon-3yWkj9Q4J-U.info.json')
+    elif title.startswith('Painkiller Already 163'):
+        return (163, 'Painkiller Already 163-CgNKKH-dC7c.info.json')
+    elif title.startswith('PKA Road to Black Ops 2'):
+        return (111, 'PKA Road to Black Ops 2 - Painkiller Already 111-Dh9BgaVn6ds.info.json')
+    elif title.startswith('Painkiller Already 99'):
+        return (99, 'Painkiller Already 99w_Ons1augh7 and Izedneck-M4PDCvH_fyE.info.json')
+    elif title.startswith('Painkiller Already Episode 56'):
+        return (56, 'Painkiller Already Episode 56.5 w_KimmyJ, ThatCoolBlackGuy-sNA248y7keE.info.json')
+
+
+    else:
+         return None
+
+def main():
+        titles = []
+        descriptions = []
+        pkaFiles = []
+        links = []
+
+
+        for i in os.listdir('json'):
+            dashCt = i.count('-') #One fname had two so need to count for slice.
+
+            titleBehavior = handleEpFromTitle(i)
+            if titleBehavior is not None:
+                pkaFiles.append(titleBehavior)
+                continue
+
+            if i.startswith(('PKA','Painkiller')):
+                words = ''.join(i.split('-')[:-dashCt])
+                titleWords = words.split()
+
+                foundInt = False
+                for j in titleWords:
+                    if foundInt:
+                        break
+                    try:
+                        if j.startswith('#'):
+                            pkaFiles.append(float(j[1:]), i)
+
+                        else:
+                            pkaFiles.append((float(j), i))
+                        break
+                    except Exception as e:
+                        pass
+
+        sortedTitles = sorted(pkaFiles, key=lambda x: (x[0], x[1].lower()), reverse=True)
+        for i in sortedTitles:
+
+            vidObject = open(f'json/{i[1]}', 'r').read()
+            vidDict = json.loads(vidObject)
+            description = vidDict['description']
+            descriptionLines = description.split('\n')
+
+            timestamps = []
+            timeLineStarted = False
+            for line in descriptionLines:
+
+                if line.strip()[0:2] == '0:' or timeLineStarted:
+                    timeLineStarted = True
+
+                    timestamps.append(line)
+
+            allTimestamps = '\t' + '\n\t'.join(timestamps)
+            descriptions.append(allTimestamps)
+            titles.append(vidDict['fulltitle'])
+            links.append(f'https://www.youtube.com/watch?v={vidDict["id"]}')
+
+
+        with open('PKA_timelines.txt', 'w+') as f:
+            for i in range(len(sortedTitles)):
+                f.write(titles[i])
+                f.write('\n' + links[i])
+                f.write('\n' + descriptions[i] + '\n\n')
+            f.write('\n')
+
+        foundEps = [i[0] for i in sortedTitles]
+        missingTimelines = []
+        for i in range(1, 516):
+            if i not in foundEps:
+                missingTimelines.append(i)
+
+        pprint(f'missing:\n{missingTimelines}')
+        print(len(sortedTitles))
 
 if __name__ == '__main__':
-    titles = []
-    descriptions = []
-    jsonFiles = [file for file in os.listdir('json')]
-    for i in jsonFiles:
-        if i.startswith(('PKA','Painkiller')):
-            words = ''.join(i.split('-')[:-1])
-            titleWords = words.split()
 
-            foundInt = False
-            for j in titleWords:
-                if foundInt:
-                    break
-                try:
-                    epNum = float(j)
-                    titles.append((epNum, i))
-                    break
-                except Exception as e:
-                    pass
-
-    sortedTitles = sorted(titles, key=lambda x: (x[0], x[1].lower()))
-    sortedTitles.reverse()
-    pprint(sortedTitles)
-    for i in sortedTitles:
-
-        vidObject = open(f'json/{i[1]}', 'r').read()
-        vidDict = json.loads(vidObject)
-        description = vidDict['description']
-        descriptionFmt = '\t' + '\n\t'.join(description.split('\n'))
-        descriptions.append('\t' + '\n\t'.join(description.split('\n')))
-
-
-    with open('descriptions.txt', 'w+') as f:
-        for i in reversed(range(len(sortedTitles))):
-            f.write(sortedTitles[i][1])
-            f.write('\n' + descriptions[i] + '\n\n')
-        f.write('\n')
-
-    foundEps = [i[0] for i in sortedTitles]
-    for i in range(1, 516):
-        if i not in foundEps:
-            print(i)
-
-    print(len(sortedTitles))
+    main()
